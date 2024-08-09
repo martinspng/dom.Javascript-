1cbff9ea36248931efc594c7031a1e104c305e23
martinspng <martins4melvis14@gmail.com>
2024-08-09 19:12:56 +0100

    my commit

---
+++ js/home.js
@@ -0,0 +1,31 @@
+alert("hello world");
+
+var visitorName = ("what is your name:");
+
+
+ console.log("visitorName");
+
+
+ var isCoolWithIt = confirm("Are you ok with it?")
+
+
+ function decodeColor( code ) {
+    if ( code === 1 ) {
+        console.log( 'Red' );
+    } else if ( code === 2 ) {
+        console.log( 'Yellow' );
+    } else if ( code === 3 ) {
+        console.log( 'Green' );
+    } else {
+        console.log( 'Unknown code' );
+    }
+}
+
+//edabit.com/challenge/QaApgtePE6QrCZ64o
+
+
+
+
+
+
+
